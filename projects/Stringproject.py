ff30538efda1dfb72c99404f029e57e43dc757b6
Shubha Sapkota <shubha.sapkota6b@gmail.com>
2026-05-25 23:36:15 +0545

    string project

---
+++ projects/Stringproject.py
@@ -0,0 +1,12 @@
+def upper(name):
+    name1= name.upper()
+    print("The uppercase of given word is ",name1)
+
+def lower(name):
+    print("The lowercase of given word is ",name.lower())
+    
+def length(name):
+    print("The length of word is "len(name))
+
+
+
