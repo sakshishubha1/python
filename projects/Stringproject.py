f36b5b9abe7630ddc7c4b7fb51cb2998418842f5
Shubha Sapkota <shubha.sapkota6b@gmail.com>
2026-05-27 19:09:33 +0545

    update

--- projects/Stringproject.py
+++ projects/Stringproject.py
@@ -9,15 +9,40 @@ def length(name):
     print("The length of word is ",len(name))
 
 def count(name):
-    character=char(input("Enter the character you want to find count of"))
+    character=input("Enter the character you want to find count of")
     print("The count of the character you entered is",name.count(character))
 
 
 def replace(name):
-    word=str(input("Enter what world do you want to replace original word with"))
+    word=input("Enter what world do you want to replace original word with")
     print("the new word is ",name.replace(name,word))
           
 def position(name):
-    character1=str(input("Enter character you want to find count of"))
+    character1=input("Enter character you want to find count of")
+    print("The position of given character is ",name.find(character1))
+
+def split(name):
+    words=name.split()
+    print(words)
+
+def start(name):
+    word1=input("enter characters you want to check")
+    print("It is ",name.startswith(word1))
+
+def end(name):
+    word2=input("enter characters you want to check")
+    print("It is ",name.endswith(word2))
     
+def comp(name):
+    word3=input("Enter word you want to compare with")
+    if name==word3:
+        print("They are equal")
+    
+    else:
+        print("They are not equal")
+    
+ 
+while True:
+    choice=int(input("What choice do you want ? 1="))
+
 
