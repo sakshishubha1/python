107c495f8ea49a6862493cba7980c726e527dd01
Shubha Sapkota <shubha.sapkota6b@gmail.com>
2026-08-18 21:19:36 +0545

    first update

---
+++ projects/list.py
@@ -0,0 +1,14 @@
+def add(number):
+    add=int(input(enter number to add))
+    number.append(add)
+
+def view(number)
+while true:
+    for num in number:
+        print(num) 
+
+def remove(number):
+    erase=int(inpt("enter number you want to delete"))
+    number.delete(remove)
+
+
