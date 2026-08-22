1637a6aa36369be60e2439efd6fad01955a946d4
Shubha Sapkota <shubha.sapkota6b@gmail.com>
2026-08-22 22:53:51 +0545

    update

--- projects/list.py
+++ projects/list.py
@@ -2,8 +2,7 @@ def add(number):
     add=int(input(enter number to add))
     number.append(add)
 
-def view(number)
-while true:
+def view(number):
     for num in number:
         print(num) 
 
@@ -17,6 +16,11 @@ def maximum(number):
 def minimum(number):
     print("the minimum number is",min(number))
 
-def avg(number)
+def avg(number):
     print("the average is ",sum(number)/len(number))
     
+def sort(number):
+    number.sort()
+    print("the sorted version is")
+    for num in number:
+        print(num)
